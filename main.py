ef0ae53a040402e1955133d583d2b9283ddd86cb
Alan Han <alan.y.han@gmail.com>
2018-07-02 23:57:09 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+
+folderPath = "C:/path/to/photos/"
+
+folders = [folderPath + folderName + "/" for folderName in os.listdir(folderPath) if not os.path.isfile(folderName)]
+count = 1
+timeOffset = 60 * 60 * 24 * 30 # all nPhoto dates are 30 days behind for some reason
+
+for folder in folders:
+    print(folder)
+
+    nPath = folder
+    iPath = folder + "iPhone/"
+
+    nPhotos = [(nFile, os.path.getmtime(nPath + nFile) + timeOffset, True)
+               for nFile in os.listdir(nPath)
+               if os.path.isfile(nPath + nFile)]
+    iPhotos = [(iFile, os.path.getmtime(iPath + iFile), False)
+               for iFile in os.listdir(iPath)
+               if os.path.isfile(iPath + iFile)]
+
+    sortedPhotos = []
+
+    while len(nPhotos) and len(iPhotos):
+        _, nTime, _ = nPhotos[0]
+        _, iTime, _ = iPhotos[0]
+
+        if nTime < iTime:
+            sortedPhotos.append(nPhotos.pop(0))
+        else:
+            sortedPhotos.append(iPhotos.pop(0))
+
+    if len(nPhotos):
+        sortedPhotos.extend(nPhotos)
+    elif len(iPhotos):
+        sortedPhotos.extend(iPhotos)
+
+    for file in sortedPhotos:
+        fileName, modTime, isNPhoto = file
+        _, fileExtension = os.path.splitext(fileName)
+
+        newName = ""
+        if isNPhoto:
+            newName = str(count).zfill(3) + "_n" + fileExtension
+            print(nPath + fileName)
+            print(nPath + newName)
+            os.rename(nPath + fileName, nPath + newName)
+        else:
+            newName = str(count).zfill(3) + "_i" + fileExtension
+            print(iPath + fileName)
+            print(nPath + newName)
+            os.rename(iPath + fileName, nPath + newName)
+
+        print("")
+
+        count += 1
